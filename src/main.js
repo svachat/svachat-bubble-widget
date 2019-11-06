b2c278f65697a5999c2c5d6031b61cfca25c0a11
Agustín <seviagus@gmail.com>
2019-11-06 12:39:38 +0100

    Add better browser support

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import vueCustomElement from 'vue-custom-element'
+import 'document-register-element/build/document-register-element'
 
 Vue.use(vueCustomElement)
 Vue.customElement('svachat-widget', App)
