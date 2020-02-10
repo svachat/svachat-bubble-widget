587337d8495a945f65d7bf66df149e691a17886e
Agus <anunez@svachat.com>
2020-02-10 15:51:08 +0100

    se añade html en los mensajes

--- src/components/MessageBubble.vue
+++ src/components/MessageBubble.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container" :class="mine ? 'mine' : 'they'" :style="cssProps">
   <div id="message-bubble" class="message-bubble" :class="mine ? 'mine' : 'they'">
-      <p>{{msg}}</p>
+      <p v-html="msg">dsdsds{{msg}}</p>
   </div>
   <div class="options" v-if="optionClicked == false">
       <p v-for="option in options" :key="option" class="option" v-on:click="sendOption( $event, option)">{{option}}</p>
