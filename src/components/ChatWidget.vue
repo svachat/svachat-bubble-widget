79b6d740ffdde43d474b173dd4260857c09c21c3
Svachat Admin <46276757+svachat-admin@users.noreply.github.com>
2021-01-19 15:05:52 +0530

    Update ChatWidget.vue

--- src/components/ChatWidget.vue
+++ src/components/ChatWidget.vue
@@ -191,7 +191,7 @@ export default {
         }
         
         //Save the user name and email only once in DB and if both are not empty only
-        console.log('this.welcomeMessageCount:'+this.userMessageCount+',sessionUserName:'+sessionUserName+',sessionUserEmail:'+sessionUserEmail+',isUserLeadDataSaved'+isUserLeadDataSaved);
+        console.log('this.welcomeMessageCount:'+this.userMessageCount+',sessionUserName:'+sessionStorage.getItem('user_name')+',sessionUserEmail:'+sessionStorage.getItem('user_email')+',isUserLeadDataSaved'+isUserLeadDataSaved);
         if(this.welcomeMessageCount>1 && sessionUserName!=null && sessionUserEmail!=null && isUserLeadDataSaved==false)
         {
           this.saveLeadData(sessionUserName,sessionUserEmail);
