e474b6554e6800a766f107d36c1155e0395728d7
Svachat Admin <46276757+svachat-admin@users.noreply.github.com>
2021-03-13 10:24:59 +0530

    Update ChatWidget.vue

--- src/components/ChatWidget.vue
+++ src/components/ChatWidget.vue
@@ -119,14 +119,7 @@ export default {
         this.toggle() 
       }
     }, 30000);
-   // this.translateTrigger();   
-   
-    var elements = document.querySelectorAll('.powered-badge');
-    console.log('ha ha my elements...');
-    console.log(elements);
-    for ( var i=elements.length; i--; ) {
-        elements[i].textContent = elements[i].textContent.replace('âš¡','⚡');
-    }
+   // this.translateTrigger(); 
   },
   methods: {
     chargeAgent() {
