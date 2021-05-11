72083e771f8912b0cdf1250a86788b9314a8b6c9
Svachat Admin <46276757+svachat-admin@users.noreply.github.com>
2021-05-11 08:43:13 +0530

    Update MessageBubble.vue

--- src/components/MessageBubble.vue
+++ src/components/MessageBubble.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container" :class="mine ? 'mine' : 'they'" :style="cssProps">
   <div id="message-bubble" class="message-bubble" :class="mine ? 'mine' : 'they'">
-      <p v-html="msg">dsdsds{{msg}}</p>
+      <p v-html="msg" v-linkified>dsdsds{{msg}}</p>
   </div>
   <div class="options" v-if="optionClicked == false">
       <p v-for="option in options" :key="option" class="option" v-on:click="sendOption( $event, option)">{{option}}</p>
@@ -10,6 +10,9 @@
 </template>
 
 <script>
+import linkify from 'vue-linkify';
+Vue.directive('linkified', linkify);
+
 export default {
     name: 'MessageBubble',
     data: function() {
