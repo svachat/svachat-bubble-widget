f543cf4856d1b7492746e98820b46d954003aa73
Svachat Admin <46276757+svachat-admin@users.noreply.github.com>
2021-05-11 13:10:43 +0530

    Update ChatWidget.vue

--- src/components/ChatWidget.vue
+++ src/components/ChatWidget.vue
@@ -77,7 +77,7 @@ export default {
       currentClient: Number,
       userLang: String,
       startChatText: 'Chat',
-      apiUrl: 'https://908348ec8b49.ngrok.io/',
+      apiUrl: 'https://042f631713ee.ngrok.io/',
       welcomeMessageCount : 0,
       userMessageCount : sessionStorage.getItem('user_msg_count')==null?0:parseInt(sessionStorage.getItem('user_msg_count')),
       errorMsg : ''
